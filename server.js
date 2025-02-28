34cd5531b20522dc18613bb2005c1a11047b3828
savannah7704 <savannah7704@comcast.net>
2025-02-27 23:22:53 -0600

    Updated files.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = proccess.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 
