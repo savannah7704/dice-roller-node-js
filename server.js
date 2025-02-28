ed0346275ef30b050b91dd0e64b36501f99b56d9
savannah7704 <savannah7704@comcast.net>
2025-02-27 22:22:39 -0600

    Add server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = proccess.env.PORT || 3000
+
+app.use(cors())
+
+app.get('/roll-die', (req, res) => {
+    const roll = Math.floor(Math.random() * 6) + 1
+    res.json({ roll })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
