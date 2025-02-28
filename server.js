617a5f716902c8da0479dcc9b01327297de8d719
savannah7704 <savannah7704@comcast.net>
2025-02-28 00:10:14 -0600

    Updated files

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
 
 app.get('/roll-die', (req, res) => {
-    const roll = Math.floor(Math.random() * 6) + 1
-    res.json({ roll })
-})
+    const roll = Math.floor(Math.random() * 6) + 1;
+    res.json({ roll });
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+    console.log(`Server running on port ${port}`);
+});
